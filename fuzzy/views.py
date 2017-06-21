dfd047a4a9cd7ff0ab7aaeac9c851d70135d196d
Michael Becker <michael.becker2@uphs.upenn.edu>
2017-06-21 16:17:37 -0400

    Update demographics to be realistic
    Different data based on sex and admit_type.

--- fuzzy/views.py
+++ fuzzy/views.py
@@ -9,54 +9,80 @@ from fuzzy import app
 FAKE = Faker()
 
 
-def random_height():
+def random_height(sex):
     """In meters"""
-    mu = 1.6
+    mu = 1.7
     sigma = 0.3
+    if sex == 'Female':
+        mu = 1.6
+        sigma = 0.2
 
-    return gauss(mu, sigma)
+    return round(gauss(mu, sigma), 3)
 
 
-def random_weight():
+def random_weight(sex):
     """In KG"""
-    mu = 80
-    sigma = 30
+    mu = 83
+    sigma = 29
+    if sex == 'Female':
+        mu = 76
+        sigma = 29
 
-    return gauss(mu, sigma)
+    return round(gauss(mu, sigma), 3)
 
 
-def random_dob():
+def random_dob(admit_type):
     """"""
-    mu = -1.6e+08
-    sigma = 6.8e+08
+    mu = -2.1e+08
+    sigma = 6.7e+08
+    if admit_type == 'Pregnancy':
+        mu = 5.5e+08
+        sigma = 1.87e+08
 
     d = datetime.date.fromtimestamp(gauss(mu, sigma))
     d = datetime.datetime(d.year, d.month, d.day)
     return d.isoformat()
 
 
-def random_admit_source():
+def random_admit_source(admit_type):
     rand = uniform(0, 1)
-    if rand < 0.5:
-        return 'EMERGENCY'
-    elif rand < 0.83:
-        return 'ROUTINE'
+
+    if admit_type == 'Pregnancy':
+        if rand < 0.12:
+            return 'ROUTINE'
+        else:
+            return 'TRANSFER'
     else:
-        return 'TRANSFER'
+        if rand < 0.5:
+            return 'EMERGENCY'
+        elif rand < 0.83:
+            return 'ROUTINE'
+        else:
+            return 'TRANSFER'
 
 
-def random_admit_type():
+def random_admit_type(sex):
     rand = uniform(0, 1)
-    if rand < 0.55:
-        return 'Emergency'
-    elif rand < 0.83:
-        return 'Routine elective'
-    elif rand < 0.93:
-        return 'Pregnancy'
-    elif rand < 0.98:
-        return 'Urgent'
+    if sex == 'Female':
+        if rand < 0.54:
+            return 'Emergency'
+        elif rand < 0.79:
+            return 'Routine elective'
+        elif rand < 0.94:
+            return 'Pregnancy'
+        elif rand < 0.98:
+            return 'Urgent'
+        else:
+            return 'Other'
     else:
-        return 'Other'
+        if rand < 0.62:
+            return 'Emergency'
+        elif rand < 0.93:
+            return 'Routine elective'
+        elif rand < 0.99:
+            return 'Urgent'
+        else:
+            return 'Other'
 
 
 def random_provider_name():
@@ -77,31 +103,33 @@ def random_hospital():
         return 'PMC'
 
 
-def random_first_name():
-    return FAKE.first_name()
+def random_first_name(sex):
+    if sex == 'Female':
+        return FAKE.first_name_female()
+    return FAKE.first_name_male()
 
 
 def random_last_name():
     return FAKE.last_name()
 
 
-def random_service():
-    rand = uniform(0, 1)
-    if rand < 0.30:
-        return 'EMERGENCY DEPARTMENT'
-    elif rand < 0.43:
-        return 'HOSPITALIST'
-    elif rand < 0.56:
-        return 'CARDIOVASCULAR MEDICINE'
-    elif rand < 0.67:
+def random_service(admit_type):
+    if admit_type == 'Pregnancy':
         return 'OBSTETRICS'
-    elif rand < 0.73:
+    rand = uniform(0, 1)
+    if rand < 0.34:
+        return 'EMERGENCY DEPARTMENT'
+    elif rand < 0.49:
+        return 'HOSPITALIST'
+    elif rand < 0.63:
+        return 'CARDIOVASCULAR MEDICINE'
+    elif rand < 0.7:
         return 'ORTHOPEDICS'
-    elif rand < 0.79:
+    elif rand < 0.77:
         return 'MEDICINE'
-    elif rand < 0.85:
+    elif rand < 0.83:
         return 'ONCOLOGY'
-    elif rand < 0.90:
+    elif rand < 0.89:
         return 'CARDIAC SURGERY'
     elif rand < 0.95:
         return 'PULMONARY'
@@ -139,40 +167,49 @@ def random_sex():
         return 'Unknown'
 
 
-def random_marital_status():
+def random_marital_status(admit_type):
     rand = uniform(0, 1)
-    if rand < 0.45:
-        return 'Single'
-    elif rand < 0.85:
-        return 'Married'
-    elif rand < 0.9:
-        return 'Widowed'
-    elif rand < 0.95:
-        return 'Divorced'
+
+    if admit_type == 'Pregnancy':
+        if rand < 0.6:
+            return 'Single'
+        else:
+            return 'Married'
     else:
-        return 'Unknown'
+        if rand < 0.43:
+            return 'Single'
+        elif rand < 0.82:
+            return 'Married'
+        elif rand < 0.89:
+            return 'Widowed'
+        elif rand < 0.95:
+            return 'Divorced'
+        else:
+            return 'Unknown'
 
 
 def random_demographic():
+    sex = random_sex()
+    admit_type = random_admit_type(sex)
     return {
-        'height': random_height(),
-        'weight': random_weight(),
-        'dob': random_dob(),
-        'admit_source': random_admit_source(),
-        'admit_type': random_admit_type(),
+        'height': random_height(sex),
+        'weight': random_weight(sex),
+        'dob': random_dob(admit_type),
+        'admit_source': random_admit_source(admit_type),
+        'admit_type': admit_type,
         'provider_name': random_provider_name(),
         'provider_phone': random_provider_phone(),
         'hospital': random_hospital(),
-        'first_name': random_first_name(),
+        'first_name': random_first_name(sex),
         'last_name': random_last_name(),
         'visit_number': random_visit_number(),
-        'service': random_service(),
+        'service': random_service(admit_type),
         'race': random_race(),
-        'gender': random_sex(),
-        'marital_status': random_marital_status(),
+        'gender': sex,
+        'marital_status': random_marital_status(admit_type),
     }
 
 
 @app.route('/v1/demographics')
 def index():
-    return flask.jsonify([random_demographic() for i in range(20)])
+    return flask.jsonify([random_demographic() for _ in range(20)])
